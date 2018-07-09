9b171c03d016071eb56489faa8b8c6d5cdf0edaf
hlay@kyriba.com <hlay@kyriba.com>
2018-07-09 19:01:56 +0200

    final implementation

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,5 +1,6 @@
 import exceptions.WithrawalNegativeValueException;
 
+import javax.annotation.Nullable;
 import java.math.BigDecimal;
 
 
@@ -12,15 +13,24 @@ public class Account
   private BigDecimal balance;
 
 
-  public Account(String name, double balance) {
+  public Account(@Nullable String name, double balance) {
     this.name = name;
     this.balance = BigDecimal.valueOf(balance);
   }
 
 
-  public void withdrawal(double value) throws WithrawalNegativeValueException {
+  /**
+   * withraw the specific amount from the balance
+   *
+   * TODO : if the implementation need to know if the withdrawal is ok or not, add a return value.
+   * It is better to add a return value if possible.
+   *
+   * @param amount the value to withdraw
+   * @throws WithrawalNegativeValueException
+   */
+  public void withdrawal(double amount) throws WithrawalNegativeValueException {
 
-    BigDecimal bgValue = BigDecimal.valueOf(value);
+    BigDecimal bgValue = BigDecimal.valueOf(amount);
 
     if (bgValue.compareTo(BigDecimal.ZERO) < 0) throw new WithrawalNegativeValueException();
 
@@ -28,6 +38,11 @@ public class Account
   }
 
 
+  /**
+   * return the balance of the account.
+   *
+   * @return the balance of the account
+   */
   public double getBalance()
   {
     return balance.doubleValue();
--- src/test/java/WithdrawalTest.java
+++ src/test/java/WithdrawalTest.java
@@ -35,6 +35,12 @@ public class WithdrawalTest
   }
 
 
+  /**
+   * As not further explanation is given, we keep the fact that we can have a negative amount.
+   * TODO : check if we need to throw an exception or to add a limit in the account
+   *
+   * @throws WithrawalNegativeValueException
+   */
   @Test
   public void withdrawalWithNotEnoughBalance() throws WithrawalNegativeValueException
   {
@@ -49,6 +55,12 @@ public class WithdrawalTest
   }
 
 
+  /**
+   * Well know problem of precision in double.
+   * in our case : 0.3d - 0.2d = 0.19999999d with double precision problem which we don't want to have
+   *
+   * @throws WithrawalNegativeValueException
+   */
   @Test
   public void withdrawalWithDoubleProblem() throws WithrawalNegativeValueException
   {
@@ -60,5 +72,47 @@ public class WithdrawalTest
 
     // then
     assertThat(account.getBalance(), is(0.1d));
+  }
+
+
+  /**
+   * 370809.77 - 879.35 - 3366790.80 will return 3139.6200000000536 if using the double problem precision
+   * @throws WithrawalNegativeValueException
+   */
+  @Test
+  public void doingMultipleWithdrawal() throws WithrawalNegativeValueException
+  {
+    // given
+    Account account = new Account("Jean-Pierre", 370809.77);
+
+    // when
+    account.withdrawal(879.35d);
+    account.withdrawal(366790.80d);
+
+    // then
+    assertThat(account.getBalance(), is(3139.62d));
+  }
+
+
+  @Test
+  public void multipleAccountWithMultipleWithdrawal() throws WithrawalNegativeValueException
+  {
+    // given
+    Account account1 = new Account("Jean-Pierre", 1000);
+    Account account2 = new Account(null, 1000);
+
+    // when
+    account1.withdrawal(100.20);
+    account1.withdrawal(150.10);
+    account1.withdrawal(200.001);
+
+
+    account2.withdrawal(1.01);
+    account2.withdrawal(5.001);
+    account2.withdrawal(1000.0001);
+
+    // then
+    assertThat(account1.getBalance(), is(549.699d));
+    assertThat(account2.getBalance(), is(-6.0111d));
   }
 }
